76eca1ceb5b4a57e247271a7a67947d9d4fc7e1d
labdogg1003 <labguitar1003@gmail.com>
2016-01-31 16:40:33 -0600

    added ellipse

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -6,12 +6,10 @@ public class Door : MonoBehaviour
     bool connected = false;
 
     //get a list of all rooms
-    ArrayList getRoomList()
+    void getRoomList()
     {
         //find our level generator : This only works if this is a child of the generator
-        LevelGenerator root = this.GetComponent<Collider>().transform.root.gameObject;
-
-        root.rooms
+//        LevelGenerator root = this.GetComponent<Collider>().transform.root.gameObject;
     }
     
 }
--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class LevelGenerator : MonoBehaviour
 {
+	bool generated;
+
 	/*
-	 *Store all the rooms we can use to build our world
+	 * Store all the rooms we can use to build our world
+	 * these will be initialized as children later.
  	 */
     public ArrayList rooms = new ArrayList();
- 	
+
+	/*
+	 * A list of the rooms that we have instantiated in our level.
+	 * These are the children of our generator.
+	 */
+	public ArrayList instantiatedRooms = new ArrayList();
+
  	/*
  	 * We are storing all the doors of every room in an array
  	 * to reduce duplication of the array for later since our
@@ -19,14 +28,24 @@ public class LevelGenerator : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-    	foreach(Room room in rooms)   
-        {
-        	foreach (Door door in room) 
-	  		{
-	   			//get all doors of all rooms
-				doors.Add(door);
-   			}
-        }
+		/* TODO : search our directory for a save file for the level
+         * This will set generated to true if we have already generated
+         * the level. for now, no check
+		 */
+
+		//Check if we have already generated the level
+		if(generated == false)
+		{
+			//First Grab a list of all our rooms : THIS WILL CAUSE ERRORS rooms != children instantitated
+    		//foreach(Room room in rooms)   
+       		//{
+        		//foreach (Door door in rooms) 
+	  			//{
+	   				//get all doors of all rooms
+					//doors.Add(door);
+   				//}
+    	    //}
+		}
 	}
 	
 	// Update is called once per frame
@@ -34,4 +53,6 @@ public class LevelGenerator : MonoBehaviour
     {
 	
 	}
+
+
 }
